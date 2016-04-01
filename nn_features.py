2c2eac8ae2b40e77aad5c68ffd1f080cf010cfcb
Mudit Bachhawat <mudit5bachhawat@gmail.com>
2016-04-01 12:08:03 +0530

    Deep Learning Model Implemented

--- nn_features.py
+++ nn_features.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 __author__ = 'mudit'
 
@@ -11,62 +13,85 @@ from configs import *
 
 logging.getLogger().setLevel(logging.INFO)
 
-df_train = pd.read_csv(INPUT_PATH + 'df_train2.csv', encoding="ISO-8859-1", index_col = 0)
+df_train = pd.read_csv(INPUT_PATH + 'df_train2.csv', encoding="ISO-8859-1", index_col=0)
+df_test = pd.read_csv(INPUT_PATH + 'df_test2.csv', encoding="ISO-8859-1", index_col=0)
 len_train = len(df_train)
+len_test = len(df_test)
 
 product_uid = np.load(FEATURES_PATH + 'product_uid.npy')
 title_vector = np.load(FEATURES_PATH + 'tfidf_svd50_product_title_vector.npy')
 description_vector = np.load(FEATURES_PATH + 'tfidf_svd50_product_description_vector.npy')
-query_vectors = np.load(FEATURES_PATH + 'tfidf_svd50_search_term_vector.npy')
-
-product_vector = np.concatenate([title_vector,description_vector],axis=1)
 
 
-product_id_to_vector = {}
-for id, prod_id in enumerate(product_uid):
-    product_id_to_vector[prod_id] = product_vector[id]
+# query_vectors = np.load(FEATURES_PATH + 'tfidf_svd50_search_term_vector.npy')
 
-# Setting offset for product_id input
-product_uid_to_nnid = {}
-for id, prod_id in enumerate(product_id_to_vector.keys()):
-    # print(id)
-    # print(prod_id)
-    product_uid_to_nnid[prod_id] = id+1
+def prepare_product_vectors(product_uid):
+    product_vector = np.concatenate([title_vector, description_vector], axis=1)
+
+    product_id_to_vector = {}
+    for id, prod_id in enumerate(product_uid):
+        product_id_to_vector[prod_id] = product_vector[id]
+
+    # Setting offset for product_id input
+    product_uid_to_nnid = {}
+    for id, prod_id in enumerate(product_id_to_vector.keys()):
+        # print(id)
+        # print(prod_id)
+        product_uid_to_nnid[prod_id] = id + 1
 
 
+    # Neural Network
+    train_product_nnid = df_train['product_uid'].map(lambda x: product_uid_to_nnid[x]).values.reshape(len_train, 1)
+    test_product_nnid = df_test['product_uid'].map(lambda x: product_uid_to_nnid[x]).values.reshape(len_test, 1)
+    # input_features = df_train[].values
+    output = df_train['relevance'].values
+    return train_product_nnid, test_product_nnid, output, product_id_to_vector, product_uid_to_nnid
 
 
-# Neural Network
-product_uid = df_train['product_uid'].map(lambda x: product_uid_to_nnid[x]).values.reshape(len_train, 1)
-# input_features = df_train[].values
-output = df_train['relevance'].values
-train_query_vectors = query_vectors[:len_train]
+def prepare_query_vectors(df_train, df_test):
+    search_term = df_train[key_query].values
+    search_term = np.concatenate([search_term, df_test[key_query].values])
+    count = TfidfVectorizer(tokenizer=lambda x: x.split(' '))
 
-pickle.dump([product_uid, output, train_query_vectors, query_vectors, np.array(product_id_to_vector.values()), product_uid_to_nnid], open(OUTPUT_PATH + 'nn.pickle','wb'))
+    count.fit(search_term)
 
-# TODO Restructure query vector Important
-#
-# graph = Graph()
-# graph.add_input(name='input_features', input_shape=(14,))
-# graph.add_input(name='product_uid', input_shape=(1,), dtype=int)
-# graph.add_input(name='query_vector', input_shape=(50,))
-# graph.add_node(Embedding(input_dim=len(prod),
-#                          output_dim=50,
-#                          weights=[product_vectors],
-#                          trainable=True,
-#                          input_length=1),
-#                name='embedding',
-#                input='product_uid', )
-# graph.add_node(Flatten(), name='flatten', input='embedding')
-# graph.add_node(Dense(50, activation='tanh'), name='hidden', inputs=['input_features', 'query_vector', 'flatten'],
-#                merge_mode="concat", concat_axis=1)
-# graph.add_node(Dense(1), name='output', input='hidden', create_output=True)
-#
-# graph.compile(optimizer=Adam(epsilon=10e-4), loss={'output': 'mse'})
-#
-# graph.fit(
-#     {'input_features': input_features, 'product_uid': product_uid, 'query_vector': train_query_vectors,
-#      'output': output},
-#     batch_size=256, nb_epoch=200, verbose=1, validation_split=0.1)
-#
-# [product_uid, input_features, output, train_query_vectors, query_vectors, prod, product_vectors] = pickle.load(open(INPUT_PATH+'nn.pickle'))
+    vocab = list(count.vocabulary_.keys())
+    sparse = count.transform(vocab)
+    tsvd = TruncatedSVD(n_components=50, random_state=configs['seed'])
+    vocab_vectors = tsvd.fit_transform(sparse)
+
+    word_to_nnindex = {}
+    for i, word in enumerate(vocab):
+        word_to_nnindex[word] = i + 1
+
+    # Converting words to indexes
+    query_indexes = []
+    for i, query in enumerate(search_term):
+        words = query.split(' ')
+        indexes = []
+        for word in words:
+            indexes.append(word_to_nnindex[word])
+        query_indexes.append(indexes)
+
+    # Add Padding to rest
+    max_words = max([len(q) for q in query_indexes])
+    for i, query_ind in enumerate(query_indexes):
+        l = len(query_ind)
+        query_indexes[i].extend([0] * (max_words - l))
+
+    query_train = np.array(query_indexes[:len(df_train)], dtype=np.int32)
+    query_test = np.array(query_indexes[len(df_train):], dtype=np.int32)
+    return vocab_vectors, query_train, query_test
+
+
+if __name__ == '__main__':
+    train_product_nnid, test_product_nnid, output, product_id_to_vector, product_uid_to_nnid = prepare_product_vectors(
+        product_uid)
+    print('- Product Vector Prepared')
+
+    vocab_vectors, query_train, query_test = prepare_query_vectors(df_train, df_test)
+    print('- Query Vector Prepared')
+
+    pickle.dump([train_product_nnid, test_product_nnid, output, np.array(product_id_to_vector.values()),
+                 product_uid_to_nnid, vocab_vectors, query_train, query_test], open(OUTPUT_PATH + 'nn.pickle', 'wb'))
+    print('- Data dumped to nn.pickle')
--- nn_train.py
+++ nn_train.py
@@ -1,49 +1,30 @@
+from configs import *
+import numpy as np
+np.random.seed(configs['seed'])
+
 import pickle
 import logging
 
 import pandas as pd
-import numpy as np
+import theano
 
-np.random.seed(2016)
+from generate_dataset import save_feature
 
-from keras.layers import Embedding, Flatten, Dense
+from keras.layers import Embedding, Flatten, Dense, LSTM
 from keras.models import Graph
-from keras.regularizers import ActivityRegularizer, WeightRegularizer, l2
-from keras.callbacks import ModelCheckpoint, Callback
-
-from configs import *
+from keras.regularizers import l2
+from keras.callbacks import Callback
 
 __author__ = 'mudit'
 
 logging.getLogger().setLevel(logging.INFO)
-[product_uid, output, train_query_vectors, query_vectors, product_vectors, prod] = pickle.load(
-    open(OUTPUT_PATH + 'nn.pickle', 'rb'))
-
-
-# output = np.load(DATASET_PATH + 'Y_train.npy')
-# df_train = pd.read_csv(INPUT_PATH + 'df_train2.csv')
-# df_test = pd.read_csv(INPUT_PATH + 'df_test2.csv')
-
-# len_train = len(df_train)
-
-# input_features = pd.DataFrame(input_features).fillna(0).values
-
-# cols = ['len_of_query', 'len_of_title',
-#         'len_of_description', 'len_of_brand',
-#         'query_in_title', 'query_in_description', 'edit_dist_in_info',
-#         'edit_ratio_in_info', 'word_in_title', 'edit_in_title',
-#         'seq_edit_in_title', 'word_in_description', 'word_in_brand',
-#         'ratio_title', 'ratio_description', 'ratio_brand', 'ngram_match_title',
-#         'ngram_match_description', 'brand_feature', 'search_term_feature']
-
-# input_features = df_train[cols].fillna(0).values
-
-# product_uid = df_train['product_uid'].values.reshape((1000, 1))
-
-# train_query_vectors = train_query_vectors[:1000]
 
 
 class SelectBestValidation(Callback):
+    def __init__(self, graph):
+        super(SelectBestValidation, self).__init__()
+        self.graph = graph
+
     def on_train_begin(self, logs={}):
         self.best_validation_score = -1
 
@@ -51,102 +32,104 @@ class SelectBestValidation(Callback):
         val_score = logs.get('val_loss')
         if self.best_validation_score == -1 or val_score <= self.best_validation_score:
             self.best_validation_score = val_score
-            self.weights = graph.get_weights()
+            self.weights = self.graph.get_weights()
             print('Saving model for val_score = %f' % val_score)
 
+def train_keras_nn():
+    r = 1e-3
+    graph = Graph()
+    # graph.add_input(name='input_features', input_shape=(input_features.shape[1],))
+    graph.add_input(name='train_product_nnid', input_shape=(1,), dtype=int)
+    graph.add_input(name='query_indexes', input_shape=(15,), dtype=int)
+    graph.add_node(Embedding(input_dim=len(prod) + 1,
+                             output_dim=100,
+                             weights=[np.concatenate((np.zeros((1, 100)), product_vectors), axis=0)],
+                             trainable=True,
+                             input_length=1,
+                             W_regularizer=l2(r)),
+                   name='embedding',
+                   input='train_product_nnid', )
+    graph.add_node(Flatten(), name='flatten', input='embedding')
 
-# product_uid = df_train['product_uid'].map(lambda x: product_id[x]).values.reshape(len_train, 1)
-# input_features = df_train[['len_of_query', 'len_of_title',
-#                            'len_of_description', 'len_of_brand',
-#                            'query_in_title', 'query_in_description', 'word_in_title',
-#                            'word_in_description', 'word_in_brand', 'ratio_title',
-#                            'ratio_description', 'ratio_brand', 'brand_feature',
-#                            'search_term_feature']].values
-# output = df_train['relevance'].values
-# train_query_vectors = query_vectors[:len_train]
-r = 0
-graph = Graph()
-# graph.add_input(name='input_features', input_shape=(input_features.shape[1],))
-graph.add_input(name='product_uid', input_shape=(1,), dtype=int)
-graph.add_input(name='query_vector', input_shape=(50,))
-graph.add_node(Embedding(input_dim=len(prod) + 1,
-                         output_dim=100,
-                         weights=[np.concatenate((np.zeros((1, 100)), product_vectors), axis=0)],
-                         trainable=True,
-                         input_length=1,
-                         W_regularizer=l2(r)),
-               name='embedding',
-               input='product_uid', )
-graph.add_node(Flatten(), name='flatten', input='embedding')
-graph.add_node(Dense(50, activation='sigmoid', W_regularizer=l2(r)), name='hidden0',
-               inputs=['query_vector', 'flatten'],
-               merge_mode="concat", concat_axis=1)
-# graph.add_node(Dense(50, activation='sigmoid'), name='hidden1', input='hidden0')
-# graph.add_node(Dense(50, activation='sigmoid'), name='hidden2', input='hidden1')
-graph.add_node(Dense(1, activation='sigmoid', W_regularizer=l2(r)), name='output', input='hidden0', create_output=True)
+    graph.add_node(Embedding(input_dim=len(vocab_vectors) + 1,
+                             output_dim=50,
+                             weights=[np.concatenate((np.zeros((1, 50)), vocab_vectors), axis=0)],
+                             trainable=True,
+                             input_length=1,
+                             W_regularizer=l2(r), mask_zero=True),
+                   name='q_embedding',
+                   input='query_indexes', )
+    lstm = LSTM(output_dim=50)
+    graph.add_node(lstm, name='LSTM', input='q_embedding')
+    graph.add_node(Dense(100, activation='sigmoid', W_regularizer=l2(r)), name='hidden0',
+                   inputs=['LSTM', 'flatten'],
+                   merge_mode="concat", concat_axis=1)
+    graph.add_node(Dense(1, activation='sigmoid', W_regularizer=l2(r)), name='output', input='hidden0', create_output=True)
 
-graph.compile(optimizer='adagrad', loss={'output': 'mse'})
-select_best = SelectBestValidation()
-# graph.fit(
-#     {'input_features': input_features, 'output': (output - 1) / 2},
-#     batch_size=256, nb_epoch=50, verbose=1, validation_split=0.5, shuffle=True)
-graph.fit(
-    {'product_uid': product_uid, 'query_vector': train_query_vectors,
-     'output': (output - 1) / 2},
-    batch_size=5000, nb_epoch=500, verbose=1, shuffle=True, callbacks=[select_best], validation_split=0.5)
+    graph.compile(optimizer='adam', loss={'output': 'mse'})
 
-quit()
+    select_best = SelectBestValidation(graph)
 
+    # To get weights of query vectors
+    get_lstm_output = theano.function([graph.inputs['query_indexes'].input], lstm.get_output(train=False))
+
+    graph.fit(
+        {'train_product_nnid': train_product_nnid, 'query_indexes': query_train,
+         'output': (output - 1) / 2},
+        batch_size=1000, nb_epoch=100, verbose=1, shuffle=True, callbacks=[select_best], validation_split=0.1)
+
+    return graph, get_lstm_output
 
 # TODO Rewrite with new structure
-def predict_on_test(df_train, df_test):
+def predict_on_test(df_train, df_test, graph, ):
     # Setting offset for product_id input
-    len_train = df_train.shape[0]
-    product_id = {}
-    k = prod.keys()
-    for id, prod_id in enumerate(k):
-        product_id[prod_id] = id + 1
+    # len_train = df_train.shape[0]
+    # product_id = {}
+    # k = prod.keys()
+    # for id, prod_id in enumerate(k):
+    #     product_id[prod_id] = id + 1
+    #
+    # test_product_uid = df_test['train_product_nnid'].map(lambda x: product_id[x]).values.reshape(len(df_test), 1)
+    # test_query_vectors = query_vectors[len_train:]
+    # test_output = df_test['relevance'].values
+    #
+    # y_pred = graph.predict_on_batch(
+    #     {'train_product_nnid': test_product_uid, 'query_vector': test_query_vectors})[
+    #     'output']
+    #
+    # y_pred = y_pred * 2 + 1
+    #
+    # y_pred = y_pred.reshape(len(df_test))
+    #
+    # pd.DataFrame({"id": df_test['id'].values, "relevance": y_pred}).to_csv('submission_nn_03_new.csv', index=False)
+    #
+    # tuned_weights = graph.get_weights()[0]
+    # # tuned_weights = tuned_weights
+    #
+    # train_X = np.concatenate((tuned_weights[train_product_nnid.reshape((len(train_product_nnid)))]), axis=1)
+    # train_X = np.concatenate((train_X, train_query_vectors), axis=1)
+    #
+    # train_Y = output
+    #
+    # test_X = np.concatenate((tuned_weights[test_product_uid.reshape((len(test_product_uid)))]), axis=1)
+    # test_X = np.concatenate((test_X, test_query_vectors), axis=1)
+    #
+    # t_X = pd.DataFrame(train_X)
+    # t_X.columns = ['feature_' + str(i) for i in range(114)]
+    # t_X['relevance'] = output
+    #
+    # t_X.to_csv(OUTPUT_PATH + 'X_features.csv', index=False)
+    #
+    # t_Y = pd.DataFrame(test_X)
+    # t_Y.columns = ['feature_' + str(i) for i in range(114)]
+    # t_Y.to_csv(OUTPUT_PATH + 'X_features_test.csv', index=False)
+    pass
 
-    test_product_uid = df_test['product_uid'].map(lambda x: product_id[x]).values.reshape(len(df_test), 1)
-    test_query_vectors = query_vectors[len_train:]
-    test_output = df_test['relevance'].values
-
-    y_pred = graph.predict_on_batch(
-        {'product_uid': test_product_uid, 'query_vector': test_query_vectors})[
-        'output']
-
-    y_pred = y_pred * 2 + 1
-
-    y_pred = y_pred.reshape(len(df_test))
-
-    pd.DataFrame({"id": df_test['id'].values, "relevance": y_pred}).to_csv('submission_nn_03_new.csv', index=False)
-
-    tuned_weights = graph.get_weights()[0]
-    # tuned_weights = tuned_weights
-
-    train_X = np.concatenate((tuned_weights[product_uid.reshape((len(product_uid)))]), axis=1)
-    train_X = np.concatenate((train_X, train_query_vectors), axis=1)
-
-    train_Y = output
-
-    test_X = np.concatenate((tuned_weights[test_product_uid.reshape((len(test_product_uid)))]), axis=1)
-    test_X = np.concatenate((test_X, test_query_vectors), axis=1)
-
-    t_X = pd.DataFrame(train_X)
-    t_X.columns = ['feature_' + str(i) for i in range(114)]
-    t_X['relevance'] = output
-
-    t_X.to_csv(OUTPUT_PATH + 'X_features.csv', index=False)
-
-    t_Y = pd.DataFrame(test_X)
-    t_Y.columns = ['feature_' + str(i) for i in range(114)]
-    t_Y.to_csv(OUTPUT_PATH + 'X_features_test.csv', index=False)
-
-
+# TODO Rewrite with new structure
 # Evaluating error entries
-def predict_evaluate_error_terms(df_train):
+def predict_evaluate_error_terms(df_train, graph):
     train_y_pred = \
-        graph.predict({'product_uid': product_uid, 'query_vector': train_query_vectors})[
+        graph.predict({'train_product_nnid': train_product_nnid, 'query_vector': query_train})[
             'output']
     train_y_pred = train_y_pred * 2 + 1
     train_y_pred = train_y_pred.reshape((len(train_y_pred)))
@@ -158,3 +141,42 @@ def predict_evaluate_error_terms(df_train):
     df_err['predicted'] = train_y_pred[high_error_indexes]
 
     df_err.to_csv(OUTPUT_PATH + 'high_error.csv', index=False)
+
+
+def save_tuned_weights(graph, get_lstm_output):
+    all_weights = graph.get_weights()
+    product_weights = all_weights[0]
+    df_all = np.concatenate([train_product_nnid, test_product_nnid])
+
+    # Reshape df_all
+    df_all.shape = len(df_all)
+    prod_vecs = product_weights[df_all]
+    save_feature(prod_vecs, 'dnn_product_vector')
+
+    query_all = np.concatenate([query_train, query_test])
+
+    # Dividing prediction into buckets (GPU out of memory)
+    query_vecs = []
+    len_all = len(query_all)
+    size = 10000
+    n = len_all/size + 1
+    for i in range(n):
+        l = get_lstm_output(query_all[size*i:size*(i+1)])
+        query_vecs.append(l)
+    query_vecs = np.concatenate(query_vecs)
+
+    # Saving Query Vectors
+    save_feature(query_vecs, 'dnn_query_vector')
+
+
+if __name__ == "__main__":
+    df_train = pd.read_csv(INPUT_PATH + 'df_train2.csv')
+    df_test = pd.read_csv(INPUT_PATH + 'df_test2.csv')
+    [train_product_nnid, test_product_nnid, output, product_vectors, prod, vocab_vectors, query_train, query_test] = pickle.load(
+        open(OUTPUT_PATH + 'nn.pickle', 'rb'))
+
+
+    model, get_lstm_output = train_keras_nn()
+    print('- Deep Net Trained')
+    save_tuned_weights(model, get_lstm_output)
+    print('- Tuned Weights Saved')
